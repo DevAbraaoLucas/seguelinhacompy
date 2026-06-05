e46ba09528c6daf4e3bcf2127101b9e6e53d4230
Abraão Lucas <luckzinrr@gmail.com>
2026-06-05 00:08:14 -0300

    atualizando codigo com proporcional e curvas 90°
    consegui integrar as curvas 90° que o proporcional não consegue fazer, alem de corrigir algumas pequenas falhas que tinham no codigo e de aumentar mais a velocidade do robô

--- linha/linha.py
+++ linha/linha.py
@@ -8,6 +8,7 @@ from pybricks.tools import wait, StopWatch
 # definindo portas de motores e sensores
 left_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B, positive_direction=Direction.CLOCKWISE)
+drive = DriveBase(left_motor, right_motor, 31, 126)
 left_sensor = ColorSensor(Port.C)
 right_sensor = ColorSensor(Port.D)
 ultrassonic_sensor = UltrasonicSensor(Port.E)
@@ -16,46 +17,51 @@ hub = PrimeHub()
 # definindo variáveis
 obstacle = 0
 
-def line_follower(Kp): # função para seguir preto e branco
-    if hub.imu.rotation(Axis.Y) > 8:  
-        base_speed = 200 # descida da rampa
-    elif hub.imu.rotation(Axis.Y) < -8: 
-        base_speed = 400 # subida da rampa
-    else:
-        base_speed = 550 # velocidade normal
+def line_follower(Kp, base_speed): # função para seguir preto e branco
+    #if hub.imu.rotation(Axis.Y) > 8:  
+    #    base_speed = 200 # descida da rampa
+    #elif hub.imu.rotation(Axis.Y) < -8: 
+    #    base_speed = 400 # subida da rampa
+    #else:
+    #    base_speed = 550 # velocidade normal
 
     if left_sensor.reflection() < 20 and right_sensor.reflection() > 40: # mtpreto branco
-        if left_sensor.reflection() < 11:
-            left_motor.run(550)
-            right_motor.run_angle(550, 216)
-            while right_sensor.reflection() > 11: # gira até o sensor direito ver preto
-                left_motor.run(-300)
-                right_motor.run(300)
-            left_motor.run(700)
-            right_motor.run(-700)
-            wait(350)
-            left_motor.run(-550)
-            right_motor.run_angle(-550, 108)
+        if right_sensor.reflection() < 20:
+            drive.drive(370, 0)
+            wait(300)
+            while right_sensor.reflection() > 25: # gira até o sensor direito ver preto
+                left_motor.dc(-60)
+                right_motor.dc(60)
+            left_motor.dc(70)
+            right_motor.dc(-70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(100)
 
-    elif left_sensor.reflection() > 40 and right_sensor.reflection() < 20: # branco mtpreto
-        if right_sensor.reflection() < 11:
-            left_motor.run(550)
-            right_motor.run_angle(550, 216)
-            while left_sensor.reflection() > 11: # gira até o sensor esquerdo ver preto
-                left_motor.run(300)
-                right_motor.run(-300)
-            left_motor.run(-700)
-            right_motor.run(700)
-            wait(350)
-            left_motor.run(-550)
-            right_motor.run_angle(-550, 108)
+    if left_sensor.reflection() > 40 and right_sensor.reflection() < 20: # branco mtpreto
+        if right_sensor.reflection() < 20:
+            drive.drive(370, 0)
+            wait(300)
+            while left_sensor.reflection() > 25: # gira até o sensor esquerdo ver preto
+                left_motor.dc(60)
+                right_motor.dc(-60)
+            left_motor.dc(-70)
+            right_motor.dc(70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(100)
 
     else:
         error = left_sensor.reflection() - right_sensor.reflection()
         correction = Kp * error
-    
-        left_motor.run(base_speed + correction)
-        right_motor.run(base_speed - correction)
+
+        left_motor_vel = base_speed + correction
+        right_motor_vel = base_speed - correction
+
+        left_motor.dc(left_motor_vel)
+        right_motor.dc(right_motor_vel)
 
 def green(): # função para fazer a verificação do verde e os três possíveis casos de verde
     left_motor.run(-550)
@@ -109,17 +115,11 @@ def green(): # função para fazer a verificação do verde e os três possívei
         right_motor.run_angle(550, 504)
 
 while True: # loop principal
-    if left_sensor.color() == Color.GREEN:
-        left_motor.run(550)
-        right_motor.run_angle(550, 36)
-        if left_sensor.color() == Color.GREEN:
+    if left_sensor.color() == Color.GREEN or right_sensor.color() == Color.GREEN:
+        left_motor.dc(50)
+        right_motor.dc(50)
+        wait(50)
+        if left_sensor.color() == Color.GREEN or right_sensor.color() == Color.GREEN:
             green()
     
-    elif right_sensor.color() == Color.GREEN:
-        left_motor.run(550)
-        right_motor.run_angle(550, 36)
-        if right_sensor.color() == Color.GREEN:
-            green()
-    
-    else:
-        line_follower(1)
+    line_follower(2, 70)
