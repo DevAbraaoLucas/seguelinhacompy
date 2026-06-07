d8e652f5c26f256bc976b7c02a012496c3cc1cfb
Abraão Lucas <luckzinrr@gmail.com>
2026-06-07 01:21:22 -0300

    quase finalizando programação de linha
    nos últimos dias, consegui adicionar a detecção dos verdes e também o desvio do obstáculo, além da fita vermelha. agora, estou quase finalizando a programação de linha pra engrenar no resgate!

--- linha/linha.py
+++ linha/linha.py
@@ -14,24 +14,16 @@ right_sensor = ColorSensor(Port.D)
 ultrassonic_sensor = UltrasonicSensor(Port.E)
 hub = PrimeHub()
 
-# definindo variáveis
-obstacle = 0
+hub.display.off()
 
 def line_follower(Kp, base_speed): # função para seguir preto e branco
-    #if hub.imu.rotation(Axis.Y) > 8:  
-    #    base_speed = 200 # descida da rampa
-    #elif hub.imu.rotation(Axis.Y) < -8: 
-    #    base_speed = 400 # subida da rampa
-    #else:
-    #    base_speed = 550 # velocidade normal
-
     if left_sensor.reflection() < 20 and right_sensor.reflection() > 40: # mtpreto branco
         if right_sensor.reflection() < 20:
             drive.drive(370, 0)
             wait(300)
             while right_sensor.reflection() > 25: # gira até o sensor direito ver preto
-                left_motor.dc(-60)
-                right_motor.dc(60)
+                left_motor.dc(-70)
+                right_motor.dc(70)
             left_motor.dc(70)
             right_motor.dc(-70)
             wait(150)
@@ -44,8 +36,8 @@ def line_follower(Kp, base_speed): # função para seguir preto e branco
             drive.drive(370, 0)
             wait(300)
             while left_sensor.reflection() > 25: # gira até o sensor esquerdo ver preto
-                left_motor.dc(60)
-                right_motor.dc(-60)
+                left_motor.dc(70)
+                right_motor.dc(-70)
             left_motor.dc(-70)
             right_motor.dc(70)
             wait(150)
@@ -64,62 +56,140 @@ def line_follower(Kp, base_speed): # função para seguir preto e branco
         right_motor.dc(right_motor_vel)
 
 def green(): # função para fazer a verificação do verde e os três possíveis casos de verde
-    left_motor.run(-550)
-    right_motor.run_angle(-550, 108)
+    left_motor.dc(-60)
+    right_motor.dc(-60)
+    wait(250)
     
     if left_sensor.color() == Color.WHITE and right_sensor.color() == Color.WHITE: # checa se tem linha antes do verde
-        left_motor.run(550)
-        right_motor.run_angle(550, 144)
+        left_motor.dc(55)
+        right_motor.dc(55)
+        wait(300)
         
         if left_sensor.color() == Color.GREEN and right_sensor.color() == Color.WHITE: # verde branco
-            left_motor.run(550)
-            right_motor.run_angle(550, 288)
-            left_motor.run(-1000)
-            right_motor.run_angle(1000, 400)
+            drive.drive(400, 0)
+            wait(444)
+            left_motor.dc(-100)
+            right_motor.dc(100)
+            wait(500)
             while right_sensor.reflection() > 20:
-                left_motor.run(-550)
-                right_motor.run(550)
-            left_motor.run(700)
-            right_motor.run_angle(-700, 300)
-            left_motor.run(-550)
-            right_motor.run_angle(-550, 72)
+                left_motor.dc(-70)
+                right_motor.dc(70)
+            left_motor.dc(70)
+            right_motor.dc(-70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(100)
             
         elif left_sensor.color() == Color.WHITE and right_sensor.color() == Color.GREEN: # branco verde
-            left_motor.run(550)
-            right_motor.run_angle(550, 288)
-            left_motor.run(1000)
-            right_motor.run_angle(-1000, 400)
+            drive.drive(400, 0)
+            wait(444)
+            left_motor.dc(100)
+            right_motor.dc(-100)
+            wait(500)
             while left_sensor.reflection() > 20:
-                left_motor.run(550)
-                right_motor.run(-550)
-            left_motor.run(-700)
-            right_motor.run_angle(700, 300)
-            left_motor.run(-550)
-            right_motor.run_angle(-550, 72)
+                left_motor.dc(70)
+                right_motor.dc(-70)
+            left_motor.dc(-70)
+            right_motor.dc(70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(100)
 
         elif left_sensor.color() == Color.GREEN and right_sensor.color() == Color.GREEN: # verde verde
-            left_motor.run(550)
-            right_motor.run_angle(550, 180)
-            left_motor.run(1000)
-            right_motor.run_angle(-1000, 1000)
-            while left_sensor.reflection() > 20:
-                left_motor.run(300)
-                right_motor.run(-300)
-            left_motor.run(-700)
-            right_motor.run_angle(700, 300)
-            left_motor.run(-550)
-            right_motor.run_angle(-550, 72)
+            drive.drive(400, 0)
+            wait(500)
+            left_motor.dc(100)
+            right_motor.dc(-100)
+            wait(1515)
+            while left_sensor.reflection() > 25:
+                left_motor.dc(75)
+                right_motor.dc(-75)
+            left_motor.dc(-70)
+            right_motor.dc(70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(50)
     
     else:
-        left_motor.run(550)
-        right_motor.run_angle(550, 504)
+        left_motor.dc(90)
+        right_motor.dc(90)
+        wait(250)
+
+def obstacle(side):
+    hub.imu.reset_heading(0)
+
+    if ultrassonic_sensor.distance() < 44:
+        if side == 1: # desvia pra esquerda
+            left_motor.dc(-100)
+            right_motor.dc(-100)
+            wait(180)
+            left_motor.dc(-100)
+            right_motor.dc(100)
+            wait(500)
+            while True:
+                drive.drive(100, 44)
+                if right_sensor.reflection() < 25:
+                    break
+            drive.drive(777, 0)
+            wait(400)
+            while True:
+                left_motor.dc(-100)
+                right_motor.dc(100)
+                if right_sensor.reflection() < 25:
+                    break
+            left_motor.dc(70)
+            right_motor.dc(-70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(400)
+            
+        elif side == 2: # desvia pra direita
+            left_motor.dc(-100)
+            right_motor.dc(-100)
+            wait(180)
+            left_motor.dc(100)
+            right_motor.dc(-100)
+            wait(500)
+            while True:
+                drive.drive(100, -44)
+                if right_sensor.reflection() < 25:
+                    break
+            drive.drive(777, 0)
+            wait(400)
+            while True:
+                left_motor.dc(100)
+                right_motor.dc(-100)
+                if right_sensor.reflection() < 25:
+                    break
+            left_motor.dc(-70)
+            right_motor.dc(70)
+            wait(150)
+            left_motor.dc(-80)
+            right_motor.dc(-80)
+            wait(400)
+
+def red_line():
+    if left_sensor.color() == Color.RED or right_sensor.color() == Color.RED:
+        drive.brake()
+        wait(99999)
 
 while True: # loop principal
+#if hub.imu.rotation(Axis.X) < -15 or hub.imu.rotation(Axis.X) > 15:
+#    line_follower(2, 50)
+#else:
+    line_follower(2, 80)
+    
     if left_sensor.color() == Color.GREEN or right_sensor.color() == Color.GREEN:
         left_motor.dc(50)
         right_motor.dc(50)
         wait(50)
         if left_sensor.color() == Color.GREEN or right_sensor.color() == Color.GREEN:
             green()
-    
-    line_follower(2, 70)
+
+    obstacle(1)
+
+    red_line()
