93838144b47de3d0360dfa6c0de4f5d65f0f04ea
Abraão Lucas <luckzinrr@gmail.com>
2026-06-28 23:54:28 -0300

    fazendo o robô levantar a garra no começo

--- resgate/resgate.py
+++ resgate/resgate.py
@@ -5,17 +5,24 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub(broadcast_channel = 94, observe_channels = [49])
+left_motor_garra = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
+right_motor_garra = Motor(Port.B, positive_direction=Direction.CLOCKWISE)
+motor_caçamba = Motor(Port.D, positive_direction=Direction.CLOCKWISE)
 
 hub.display.off()
 hub.light.off() # desligando as luzes do hub pra economizar bateria
 
-while True:
-    print(hub.battery.voltage())
-    
+def movimento_garra(speed, time):
+    left_motor_garra.dc(speed)
+    right_motor_garra.dc(speed)
+    wait(time)
+    left_motor_garra.brake()
+    right_motor_garra.brake()
+
+while True:    
     data = hub.ble.observe(49)
-
-    if data == 12:
-        hub.light.on(Color.RED)
-
+    
+    if data == 0:
+        movimento_garra(67, 500)
     else:
-        hub.light.on(Color.GREEN) 
+        wait(6.7)
